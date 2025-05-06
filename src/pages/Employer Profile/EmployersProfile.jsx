a01b9d18a9d64b28ea28a36f4a15bb04a48f2e15
Leotrim Halimi <leootrim221@gmail.com>
2025-05-06 17:49:08 +0200

    reset to default state

--- src/pages/Employer Profile/EmployersProfile.jsx
+++ src/pages/Employer Profile/EmployersProfile.jsx
@@ -165,6 +165,15 @@ export default function EmployersProfile() {
     }));
   };
 
+  function enableEditingUserProfile() {
+    if (isEditEnabled) {
+      setUpdatedUserData(userProfile)
+      setEditEnable(false)
+      return
+    }
+    setEditEnable(true)
+  }
+
 
   return (
     <>
@@ -206,7 +215,7 @@ export default function EmployersProfile() {
               <DeleteAlert isOpen={showModal} onClose={() => setShowModal(false)} onDeleteUser={onDeleteUser} />
               <button
                 type="button"
-                onClick={() => setEditEnable(!isEditEnabled)}
+                onClick={enableEditingUserProfile}
                 className="inline-flex items-center justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 Edit
