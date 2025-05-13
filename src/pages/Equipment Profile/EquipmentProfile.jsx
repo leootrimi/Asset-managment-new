87f12abf2765f44de461e1ede924d597029d98e8
Leotrim Halimi <leootrim221@gmail.com>
2025-05-13 09:34:14 +0200

    update equipment activity

--- src/pages/Equipment Profile/EquipmentProfile.jsx
+++ src/pages/Equipment Profile/EquipmentProfile.jsx
@@ -3,8 +3,6 @@
 import { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import {
-  Dialog,
-  DialogPanel,
   Label,
   Listbox,
   ListboxButton,
@@ -16,7 +14,6 @@ import {
   MenuItems,
 } from '@headlessui/react'
 import {
-  Bars3Icon,
   CalendarDaysIcon,
   CreditCardIcon,
   EllipsisVerticalIcon,
@@ -32,12 +29,6 @@ import {
 import { CheckCircleIcon } from '@heroicons/react/24/solid'
 import { apiRequest } from '../../services/ApiCalls'
 
-const navigation = [
-  { name: 'Home', href: '#' },
-  { name: 'Invoices', href: '#' },
-  { name: 'Clients', href: '#' },
-  { name: 'Expenses', href: '#' },
-]
 const invoice = {
   subTotal: '$8,800.00',
   tax: '$1,760.00',
@@ -77,24 +68,7 @@ const invoice = {
     },
   ],
 }
-const activity = [
-  { id: 1, type: 'created', person: { name: 'Chelsea Hagon' }, date: '7d ago', dateTime: '2023-01-23T10:32' },
-  { id: 2, type: 'edited', person: { name: 'Chelsea Hagon' }, date: '6d ago', dateTime: '2023-01-23T11:03' },
-  { id: 3, type: 'sent', person: { name: 'Chelsea Hagon' }, date: '6d ago', dateTime: '2023-01-23T11:24' },
-  {
-    id: 4,
-    type: 'commented',
-    person: {
-      name: 'Chelsea Hagon',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    comment: 'Called client, they reassured me the invoice would be paid by the 25th.',
-    date: '3d ago',
-    dateTime: '2023-01-23T15:56',
-  },
-  { id: 5, type: 'viewed', person: { name: 'Alex Curren' }, date: '2d ago', dateTime: '2023-01-24T09:12' },
-  { id: 6, type: 'paid', person: { name: 'Alex Curren' }, date: '1d ago', dateTime: '2023-01-24T09:20' },
+let activity = [
 ]
 const moods = [
   { name: 'Excited', value: 'excited', icon: FireIcon, iconColor: 'text-white', bgColor: 'bg-red-500' },
@@ -135,6 +109,12 @@ function equipmentStatus(employer) {
   }
 }
 
+function formatDate(date) {
+  const isoDate = new Date(date);
+  return isoDate.toLocaleDateString();
+}
+
+
 
 export default function EquipmentProfile() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -147,7 +127,8 @@ export default function EquipmentProfile() {
       try {
         const response = await apiRequest({ endpoint: `/equipments/${id}` });
         setEquipmentsProfile(response)
-        console.log(response);
+        activity = response.activity
+        console.log(activity);
 
       } catch (error) {
         console.log(error);
@@ -167,7 +148,7 @@ export default function EquipmentProfile() {
                   clipPath:
                     'polygon(100% 38.5%, 82.6% 100%, 60.2% 37.7%, 52.4% 32.1%, 47.5% 41.8%, 45.2% 65.6%, 27.5% 23.4%, 0.1% 35.3%, 17.9% 0%, 27.7% 23.4%, 76.2% 2.5%, 74.2% 56%, 100% 38.5%)',
                 }}
-                className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#FF80B5] to-[#9089FC]"
+                className="aspect-[1154/678] w-[72.125rem] bg-gradient-to-br from-[#3483eb] to-[##3483eb]"
               />
             </div>
             <div className="absolute inset-x-0 bottom-0 h-px bg-gray-900/5" />
@@ -445,43 +426,17 @@ export default function EquipmentProfile() {
                     >
                       <div className="w-px bg-gray-200" />
                     </div>
-                    {activityItem.type === 'commented' ? (
-                      <>
-                        <img
-                          alt=""
-                          src={activityItem.person.imageUrl}
-                          className="relative mt-3 size-6 flex-none rounded-full bg-gray-50"
-                        />
-                        <div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200">
-                          <div className="flex justify-between gap-x-4">
-                            <div className="py-0.5 text-xs/5 text-gray-500">
-                              <span className="font-medium text-gray-900">{activityItem.person.name}</span> commented
-                            </div>
-                            <time dateTime={activityItem.dateTime} className="flex-none py-0.5 text-xs/5 text-gray-500">
-                              {activityItem.date}
-                            </time>
-                          </div>
-                          <p className="text-sm/6 text-gray-500">{activityItem.comment}</p>
-                        </div>
-                      </>
-                    ) : (
-                      <>
-                        <div className="relative flex size-6 flex-none items-center justify-center bg-white">
-                          {activityItem.type === 'paid' ? (
-                            <CheckCircleIcon aria-hidden="true" className="size-6 text-indigo-600" />
-                          ) : (
-                            <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-                          )}
-                        </div>
-                        <p className="flex-auto py-0.5 text-xs/5 text-gray-500">
-                          <span className="font-medium text-gray-900">{activityItem.person.name}</span>{' '}
-                          {activityItem.type} the invoice.
-                        </p>
-                        <time dateTime={activityItem.dateTime} className="flex-none py-0.5 text-xs/5 text-gray-500">
-                          {activityItem.date}
-                        </time>
-                      </>
-                    )}
+
+                    <div className="relative flex size-6 flex-none items-center justify-center bg-white">
+                      <div className="size-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
+                    </div>
+                    <p className="flex-auto py-0.5 text-xs/5 text-gray-500">
+                      <span className="font-medium text-gray-900">{activityItem.user}</span>{' '}
+                      {activityItem.activity}.
+                    </p>
+                    <time dateTime={activityItem.dateTime} className="flex-none py-0.5 text-xs/5 text-gray-500">
+                      {formatDate(activityItem.date)}
+                    </time>
                   </li>
                 ))}
               </ul>
