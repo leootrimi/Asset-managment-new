35425b3300406cb1f0e467cc2fc6e55bdbf29f57
Leotrim Halimi <leootrim221@gmail.com>
2025-05-19 13:32:47 +0200

    change contnet

--- src/pages/Landing Page/Landing.jsx
+++ src/pages/Landing Page/Landing.jsx
@@ -156,11 +156,11 @@ export default function Landing() {
           </div>
           <div className="text-center">
             <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-              Data to enrich your online business
+              Empower Your Team with PrimeNest
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat.
+              Simplify equipment tracking, streamline asset requests, and stay on top of user assignments —
+               all in one intuitive dashboard designed for modern organizations.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
